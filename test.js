9a3a8800e71894b1a720c404bbc3a9fe720a36ce
AJ ONeal <coolaj86@gmail.com>
2012-02-10 00:26:23 -0700

    tested session more thoroughly and fixed bugs

--- test.js
+++ test.js
@@ -9,17 +9,17 @@
     , hostname = 'localhost'
     , port = '6767'
     , pathname = '/path/to/resource'
-    , search = 'search=query&foo=bar'
+    , search = 'search=query&foo=bar&userSession=alphabetSoup'
     , url = pathname + '?' + search
     , fullurl = protocol + '//' + hostname + ':' + port + pathname + '?' + search
     , query = {
           search: 'query'
         , foo: 'bar'
+        , userSession: 'alphabetSoup'
       }
     ;
 
   server = connect.createServer(function (req, res, next) {
-    console.log(req.url);
     res.json({
         "url": req.url
       //, "adddress": req.socket.address()
@@ -28,18 +28,32 @@
       , "query": req.query
       //, "search": req.search
       , "path": req.path
+      , "headers": req.headers
     });
   });
 
   server.listen(port, function () {
-    request.get(fullurl).when(function (err, ahr, data) {
+    var headers
+      ;
+
+    headers = {
+        "X-User-Session": "headerSession"
+      , "User-Agent": "FooBarUrl"
+    };
+
+    request.get(fullurl, null, { headers: headers }).when(function (err, ahr, resp) {
+      var data
+        ;
+
       server.close();
       if (err) {
         console.error(err);
         return;
       }
 
-      data = data.result;
+      data = resp.result;
+      assert.strictEqual("headersession", resp.userSession, "sessions aren't a lowercase match");
+      assert.strictEqual("foobarurl", data.headers['user-agent'], 'user-agent is a lowercase match');
       assert.strictEqual(url, data.url, "urls don't match");
       //assert.strictEqual(protocol, data.protocol, "protocol don't match");
       //assert.strictEqual(hostname, data.hostname, "hostnames don't match");
