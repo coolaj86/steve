f2a01e519b016ab796f6b547bd2003223896aebe
AJ ONeal <coolaj86@gmail.com>
2012-02-09 22:00:33 -0700

    made the appropriate fix this time

--- test.js
+++ test.js
@@ -3,21 +3,51 @@
   
   var connect = require('./lib/index')
     , request = require('ahr2')
+    , assert = require('assert')
     , server
+    , protocol = 'http:'
+    , hostname = 'localhost'
+    , port = '6767'
+    , pathname = '/path/to/resource'
+    , search = 'search=query&foo=bar'
+    , url = pathname + '?' + search
+    , fullurl = protocol + '//' + hostname + ':' + port + pathname + '?' + search
+    , query = {
+          search: 'query'
+        , foo: 'bar'
+      }
     ;
 
-  server = connect.createServer();
+  server = connect.createServer(function (req, res, next) {
+    console.log(req.url);
+    res.json({
+        "url": req.url
+      //, "adddress": req.socket.address()
+      //, "protocol": req.protocol
+      , "pathname": req.pathname
+      , "query": req.query
+      //, "search": req.search
+      , "path": req.path
+    });
+  });
 
-  server.listen(6767, function () {
-    request.get('http://localhost:6767/').when(function (err, ahr, data) {
+  server.listen(port, function () {
+    request.get(fullurl).when(function (err, ahr, data) {
       server.close();
       if (err) {
         console.error(err);
         return;
       }
 
-      console.info('data:');
-      console.log(data.toString('utf8'));
+      data = data.result;
+      assert.strictEqual(url, data.url, "urls don't match");
+      //assert.strictEqual(protocol, data.protocol, "protocol don't match");
+      //assert.strictEqual(hostname, data.hostname, "hostnames don't match");
+      //assert.strictqual(port, data.port, "ports don't match");
+      assert.strictEqual(pathname, data.pathname, "pathnames don't match");
+      //assert.strictEqual(search, data.search, "searchs don't match");
+      assert.deepEqual(query, data.query, "queries don't match");
+      console.log('tests pass (ctrl+c to exit)');
     });
   });
 
